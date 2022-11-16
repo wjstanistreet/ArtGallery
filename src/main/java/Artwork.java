af36969ab41b9310e67c4fee36a42193a8f7b806
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-16 16:00:13 +0000

    added artwork

--- src/main/java/Artwork.java
+++ src/main/java/Artwork.java
@@ -1,2 +1,45 @@
 public class Artwork {
+    private String title;
+    private Artist artist;
+    private Float price;
+    private String nft;
+
+    public Artwork(String title, Artist artist, Float price, String nft){
+        this.title = title;
+        this.artist = artist;
+        this.price = price;
+        this.nft = nft;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Artist getArtist() {
+        return this.artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public Float getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getNft(){
+        return this.nft;
+    }
+
+    public void setNft(String nft) {
+        this.nft = nft;
+    }
 }
