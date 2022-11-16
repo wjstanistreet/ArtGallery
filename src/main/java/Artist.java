ee10696ffbe1e1267c7afd789c3d6392619fdde2
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-16 15:51:52 +0000

    added artist class

--- src/main/java/Artist.java
+++ src/main/java/Artist.java
@@ -1,2 +1,15 @@
 public class Artist {
+    private String name;
+
+    public Artist(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
